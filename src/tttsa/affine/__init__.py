7cdad6211bc3fb6fad1e5fbca931a6cc02907789
McHaillet <martenchaillet@gmail.com>
2024-11-14 18:35:55 +0100

    refactor: remoeve image stretch function

--- src/tttsa/affine/__init__.py
+++ src/tttsa/affine/__init__.py
@@ -1,9 +1,8 @@
 """2D and 3D affine transform functionality."""
 
-from .affine_transform import affine_transform_2d, affine_transform_3d, stretch_image
+from .affine_transform import affine_transform_2d, affine_transform_3d
 
 __all__ = [
     "affine_transform_2d",
     "affine_transform_3d",
-    "stretch_image",
 ]
