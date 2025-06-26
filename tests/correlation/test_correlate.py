36cb9f6968c79709461dd470f9d20ac32839102d
McHaillet <martenchaillet@gmail.com>
2025-06-26 13:11:36 +0200

    fix tests with latest changes

--- tests/correlation/test_correlate.py
+++ tests/correlation/test_correlate.py
@@ -8,14 +8,13 @@ def test_correlate_2d():
     a[5, 5] = 1
     b = torch.zeros((10, 10))
     b[6, 6] = 1
-    cross_correlation = correlate_2d(a, b, normalize=True)
+    cross_correlation = correlate_2d(a, b)
     peak_position = torch.unravel_index(
         indices=torch.argmax(cross_correlation), shape=cross_correlation.shape
     )
     shift = torch.as_tensor(peak_position) - torch.tensor([5, 5])
     assert peak_position == (4, 4)
     assert torch.allclose(shift, torch.tensor([-1, -1]))
-    assert torch.allclose(cross_correlation[peak_position], torch.tensor([1.0]))
     assert cross_correlation.shape == a.shape
 
 
@@ -25,9 +24,9 @@ def test_correlate_2d_stacks():
     a[:, 5, 5] = 1
     b = torch.zeros((1, 10, 11))
     b[:, 6, 6] = 1
-    cross_correlation = correlate_2d(a, b, normalize=True)
+    cross_correlation = correlate_2d(a, b)
     assert cross_correlation.shape == a.shape
-    cross_correlation = correlate_2d(b, a, normalize=True)
+    cross_correlation = correlate_2d(b, a)
     assert cross_correlation.shape == a.shape
-    cross_correlation = correlate_2d(a, a, normalize=True)
+    cross_correlation = correlate_2d(a, a)
     assert cross_correlation.shape == a.shape
